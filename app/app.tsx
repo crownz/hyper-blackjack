a22339c8222845f301e6b675625776795c127131
Vytautas Perliba <v.perliba@kootium.com>
2018-03-10 12:48:08 +0200

    added first game state

--- app/app.tsx
+++ app/app.tsx
@@ -1,10 +1,49 @@
 import { h, app } from 'hyperapp';
-import './app.css';
 
-const view = () => (
-  <div>
-    Welcome.
+import Home from './pages/home';
+
+import * as Styles from './app.css';
+
+enum GameState {
+  NotStarted = 'NotStarted',
+  Started = 'Started',
+  Finished = 'Finished',
+}
+
+interface State {
+  gameState: GameState;
+}
+
+interface Actions {
+  changeGameState: (value: GameState) => void;
+}
+
+const appState: State = {
+  gameState: GameState.NotStarted,
+};
+
+const appActions: Actions = {
+  changeGameState: (value: GameState) => (state: State) => ({ gameState: value }),
+};
+
+const renderGame = (gameState: GameState, changeGameState: (value: GameState) => void) => {
+  switch (gameState) {
+    case GameState.NotStarted:
+      return <Home startGame={() => changeGameState(GameState.Started)} />;
+    case GameState.Started:
+      return <div>Started!</div>;
+    default:
+      return <Home startGame={() => changeGameState(GameState.Started)} />;
+  }
+}
+
+const view = (state: State, actions: Actions) => (
+  <div class={Styles.container}>
+    <div class={Styles.title}>
+      Welcome to HyperBlackjack.
+    </div>
+    {renderGame(state.gameState, actions.changeGameState)}
   </div>
 )
 
-app({}, {}, view, document.body);
+app(appState, appActions, view, document.body);
---
+++ app/pages/home/home.tsx
@@ -0,0 +1,14 @@
+import { h } from "hyperapp"
+import * as Styles from './home.css';
+
+interface Props {
+  startGame: () => void;
+}
+
+const Home = ({ startGame }: Props) => (
+  <div class={Styles.container}>
+    <button onclick={startGame}>START</button>
+  </div>
+);
+
+export default Home;
